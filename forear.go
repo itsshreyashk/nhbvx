ea4e1eefdae39a2be79767be3b2852478bedaaf8
Shreyash Kumar <shreyashkumar.me@gmail.com>
2024-04-15 23:46:10 +0530

    way to go

---
+++ forear.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+
+func sample ()  {
+	fmt.Print("Hello World")
+}
--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-    "fmt"
-	"net/http"
+	"fmt"
 	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+	"os"
+
+	"context"
+	"encoding/json"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 )
 
-
-func main ()  {
+func main() {
 	r := mux.NewRouter()
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.HandleFunc("/tinyurl/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		fmt.Printf("as %v", id)
+	}).Methods("GET")
+	r.HandleFunc("/shorten/{url}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+        url := vars["url"]
+        fmt.Printf("as %v", url)
+	})
 	fmt.Println("Listening...")
-    http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", r)
 }
