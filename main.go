e64810829b7b158e34e26b0a5b25793fdcba6bae
Shreyash Kumar <shreyashkumar.me@gmail.com>
2024-04-16 10:27:31 +0530

    almost

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
 	"log"
+	"net/http"
 	"os"
 
-	"context"
-	"encoding/json"
-
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
+	// "context"
+	// "encoding/json"
+	// "github.com/joho/godotenv"
+	// "go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err!= nil {
+        log.Fatal("Error loading.env file")
+    }
+	url := os.Getenv("MONGODB_URI")
+	if url == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
 	r := mux.NewRouter()
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	r.HandleFunc("/tinyurl/{id}", func(w http.ResponseWriter, r *http.Request) {
