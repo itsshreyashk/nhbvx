2b64a2c0ea4921ed7019885feee65ac9f0d4e852
Shreyash Kumar <shreyashkumar.me@gmail.com>
2024-04-16 10:35:37 +0530

    ...

--- main.go
+++ main.go
@@ -7,13 +7,12 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
-	// "context"
+	"context"
 	// "encoding/json"
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	// "go.mongodb.org/mongo-driver/bson"
-	// "go.mongodb.org/mongo-driver/mongo"
-	// "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -21,10 +20,33 @@ func main() {
 	if err!= nil {
         log.Fatal("Error loading.env file")
     }
-	url := os.Getenv("MONGODB_URI")
-	if url == "" {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
+
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err!= nil {
+        log.Panic(err)
+    }
+
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+
+	coll := client.Database("nhbvx").Collection("mappings")
+
+
+	fmt.Printf("Coll Type : %T", coll)
+
+
+	/* Creating Router */
 	r := mux.NewRouter()
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	r.HandleFunc("/tinyurl/{id}", func(w http.ResponseWriter, r *http.Request) {
